de79adc86393b687a93ac0c5c1de4a3ba7b4f507
adamschoelz <adamschoelz@gmail.com>
2015-10-04 18:02:03 -0500

    Added checkPalindrome and findLongestPalindrome functions

--- AlgorithmsAssignment2/src/AlgorithmsAssignment2A.java
+++ AlgorithmsAssignment2/src/AlgorithmsAssignment2A.java
@@ -5,5 +5,26 @@ public class AlgorithmsAssignment2A {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public static boolean checkPalindrome(String str){
+		boolean toReturn = true;
+		
+		for(int i = 0; i < str.length(); i++){
+			if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
+				toReturn = false;
+				break;
+			}
+		}
+		
+		return toReturn;
+	}
 
+	public static String findLongestPalindrome(String str){
+		
+		for(int i = 0; i < str.length(); i++){
+			
+		}
+		
+		return null;
+	}
 }
